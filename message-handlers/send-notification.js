b7294b81c254e5495b902d4ad76591adc0126f96
leeran <leeran@sears.co.il>
2018-01-03 16:47:53 +0200

    test

--- message-handlers/send-notification.js
+++ message-handlers/send-notification.js
@@ -2,7 +2,7 @@ const sendGenericMessage = require('../message-handlers/send-generic-message');
 
 function sendNotification(bot, chatId, floor) {
     const joke = getRandomJoke();
-    const message = joke + '\n\n' + 'Your food is on floor ' + floor + '! :raised_hands: Bon appetit! :fork_and_knife:';
+    const message = joke + '\n\n' + 'Your food is on floor '/* + floor*/ + '! :raised_hands: Bon appetit! :fork_and_knife:';
 
     sendGenericMessage(bot, chatId, message);
 }
