aa2e4bae54ceab2652e2b09dd4fbeaf70a832007
leeran <leeran@sears.co.il>
2018-01-03 10:23:00 +0200

    add enpoint for notification sending

---
+++ send-notification.js
@@ -0,0 +1,18 @@
+function sendNotification(bot, userId) {
+    const chatId = userId;
+    const text = 'If we are not meant to have midnight snacks, why is there a light in the fridge?';
+    const options = {
+        parse_mode: 'Markdown',
+        reply_markup: {
+            keyboard: [[{ text: 'I\'m hungry' }]],
+            resize_keyboard: false,
+            one_time_keyboard: false
+        }
+    };
+
+    bot.sendMessage(chatId, text, options).then(function () {
+
+    });
+}
+
+module.exports = sendNotification;
--- web.js
+++ web.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var packageInfo = require('./package.json');
 var bodyParser = require('body-parser');
+var sendNotification = require('./send-notification');
 
 var app = express();
 app.use(bodyParser.json());
@@ -21,6 +22,14 @@ module.exports = function (bot) {
     console.log(JSON.stringify(req.body));
     
     bot.processUpdate(req.body);
+    res.sendStatus(200);
+  });
+
+  app.post('/notify', function (req, res) {
+    console.log('Notification recieved from web app.');
+    console.log(JSON.stringify(req.body));
+
+    sendNotification(bot, req.body.userId);
     res.sendStatus(200);
   });
 };
