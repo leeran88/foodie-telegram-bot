8670bf2d2fdb4f43717d7fe92d70e1b9e293f916
leeran <leeran@sears.co.il>
2018-01-03 16:53:50 +0200

    fix emoji

--- message-handlers/send-notification.js
+++ message-handlers/send-notification.js
@@ -2,17 +2,17 @@ const sendGenericMessage = require('../message-handlers/send-generic-message');
 
 function sendNotification(bot, chatId, floor) {
     const joke = getRandomJoke();
-    const message = joke + '\n\n' + 'Your food is on floor '/* + floor*/ + '! :raised_hands: Bon appetit! :fork_and_knife:';
+    const message = joke + '\n\n' + 'Your food is on floor '/* + floor*/ + '! :raised_hands: Bon appetit! 🍴';
 
     sendGenericMessage(bot, chatId, message);
 }
 
 function getRandomJoke() {
     const jokes = [
-        'If we are not meant to have midnight snacks, why is there a light in the fridge :flashlight:?',
-        'I\'m just a girl standing in front of a salad, asking it to be a donut. :doughnut:',
-        'Q:  What did one steak knife say to the other :hocho:?' + '\n' + 'A:  Look sharp, here comes the meat. :poultry_leg:',
-        'Q:  Why did the tomato turn red :tomato:?' + '\n' + 'A:  Because it saw the salad dressing. :dress:',
+        'If we are not meant to have midnight snacks, why is there a light in the fridge 🔦?',
+        'I\'m just a girl standing in front of a salad, asking it to be a donut. 🍩',
+        'Q:  What did one steak knife say to the other 🔪?' + '\n' + 'A:  Look sharp, here comes the meat. 🍗',
+        'Q:  Why did the tomato turn red 🍅?' + '\n' + 'A:  Because it saw the salad dressing. 👗',
     ];
 
     const jokeIndex = Math.floor(Math.random() * jokes.length);
