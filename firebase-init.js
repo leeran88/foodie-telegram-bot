c73bf4b8013f6f4cfd0f5e67921fe291950dfc17
leeran <leeran@sears.co.il>
2018-01-03 11:35:10 +0200

    refactor

---
+++ firebase-init.js
@@ -0,0 +1,16 @@
+const firebaseInit = require('firebase');
+
+function firebaseInit() {
+    const firebaseConfig = {
+        apiKey: process.env.FIREBASE_API_KEY,
+        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+        databaseURL: process.env.FIREBASE_DB_URL,
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+    };
+    
+    firebase.initializeApp(firebaseConfig);
+}
+
+module.exports = firebaseInit;
